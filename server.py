57d3350a59ee870c08c987485532ccc43d5f336f
M. Phil Lowney <mlowney@umassd.edu>
2016-11-16 19:55:38 -0500

    Implement threading on server

--- server.py
+++ server.py
@@ -2,50 +2,54 @@
 # Section 2.7.2 TCP Socket Programming page 167
 
 from socket import *
+import sys
+import thread
 
-serverPort = 4321
+def clientConnection(connectionSocket, addr):
+    print("Connection Received: "+addr[0])
+    s = open('small','rb')
+    m = open('medium', 'rb')
+    b = open('medium', 'rb')
+    connected = 1;
+    while connected:
+        try:
+            req = connectionSocket.recv(1024)
+            print("Received a request for "+req)
+            if req == 't':
+                connectionSocket.send("YOU GET NOTHING")
+            elif req == 's':
+                l = s.read(1024*1024)
+                while(l):
+                    print("Sending a picture")
+                    connectionSocket.send(l)
+                    l = s.read(1024*1024)
+            elif req ==  'm':
+                l = m.read(1024)
+                while(l):
+                    print("Sending a book")
+                    connectionSocket.send(l)
+                    l = m.read(1024)
+            elif req ==  'b':
+                l = b.read(1024)
+                while(l):
+                    print("Sending an operating system")
+                    connectionSocket.send(l)
+                    l = b.read(1024)
+            elif req == 'q':
+                connectionSocket.close()
+                connected = 0
+            print("Done Sending")
+        except:
+            connectionSocket.close()
+
+running = 1
+serverPort = 1234
 serverSocket = socket(AF_INET,SOCK_STREAM)
-
 serverSocket.bind(('',serverPort))
 
-s = open('animals.jpg','rb')
-m = open('realbook.pdf', 'rb')
-b = open('en_windows_10_education_version_1607_updated_jul_2016_x86_dvd_9055803.iso', 'rb')
 
-serverSocket.listen(1)	# serverSocket is the welcoming Connection
+serverSocket.listen(5)	# serverSocket is the welcoming Connection
 
-print 'The server is ready to receive'
-
-while 1:
-    connectionSocket, addr = serverSocket.accept()
-    try:
-        print("Connection Received: "+addr[0])
-
-        req = connectionSocket.recv(1024)
-        print("Received a request for "+req)
-        if req == 't':
-            connectionSocket.send("YOU GET NOTHING")
-            print("Got here")
-        elif req == 's':
-            l = s.read(1024*1024)
-            while(l):
-                print("Sending a picture")
-                connectionSocket.send(l)
-                l = s.read(1024*1024)
-        elif req ==  'm':
-            l = m.read(1024)
-            while(l):
-                print("Sending a book")
-                connectionSocket.send(l)
-                l = m.read(1024)
-        elif req ==  'b':
-            l = b.read(1024)
-            while(l):
-                print("Sending an operating system")
-                connectionSocket.send(l)
-                l = b.read(1024)
-
-        print("Done Sending")
-        connectionSocket.close()
-    except:
-        connectionSocket.close()
+while running:
+    print("The server is ready to receive")
+    thread.start_new_thread(clientConnection, serverSocket.accept())
